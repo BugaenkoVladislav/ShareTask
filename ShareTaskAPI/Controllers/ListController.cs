339db074713dd6d90361329908284b3146c4c1a4
https://github.com/BugaenkoVladislav <bugaenkovlad201@gmail.com>
2023-11-12 17:52:05 +0300

    migration folder

---
+++ ShareTaskAPI/Controllers/ListController.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShareTaskAPI.Context;
+using ShareTaskAPI.Entities;
+using ShareTaskAPI.Service;
+
+namespace ShareTaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Policy = "Authorized")]
+    public class ListController : ControllerBase
+    {
+        private MyDbContext _db;
+        public ListController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("AddList")]
+        public IActionResult AddList([FromBody]List list)
+        {
+            try
+            {
+                var user = AccountActions.ReturnUserFromCookie(this.HttpContext, _db);
+                _db.Lists.Add(new List()
+                {
+                    Name = list.Name,
+                    IsPublic = list.IsPublic,
+                    Description = list.Description,
+                    IdCreator = user.IdUser,
+                    Linq = list.Linq
+                });
+                _db.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("GetMyLists")]
+        public IActionResult GetMyLists()
+        {
+            try
+            {
+                var user = AccountActions.ReturnUserFromCookie(this.HttpContext, _db).IdUser;
+                var list = _db.Lists.Where(x => x.IdCreator == user).ToList();
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("GetMyList/{name}")]
+        public IActionResult GetMyList([FromRoute] string name)
+        {
+            try
+            {
+                var user = AccountActions.ReturnUserFromCookie(this.HttpContext, _db).IdUser;
+                Entities.List? list = _db.Lists.FirstOrDefault(x => x.Name == name && x.IdCreator == user);
+                if (list == null)
+                    return NotFound("list not found");
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteMyList/{name}")]
+        public IActionResult DeleteMyList([FromRoute] string name)
+        {
+            try
+            {
+                var user = AccountActions.ReturnUserFromCookie(this.HttpContext, _db);
+                var list = _db.Lists.FirstOrDefault(x => x.Name == name && x.IdCreator == user.IdUser);
+                if (list == null)
+                    return NotFound("list not found");
+                _db.Lists.Remove(list);
+                _db.SaveChanges();
+                return Ok("Success");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ ShareTaskAPI/Migrations/20231112081953_AddLists.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ShareTaskAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLists : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lists",
+                columns: table => new
+                {
+                    idList = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    isPublic = table.Column<bool>(type: "boolean", nullable: false),
+                    linq = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Lists_pkey", x => x.idList);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lists");
+        }
+    }
+}
